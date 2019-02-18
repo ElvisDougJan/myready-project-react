20e45c21c5c6df3c46029f29682bcaf0c553110b
Elvis Douglas Janegitz <edjelvis@gmail.com>
2019-02-17 21:52:30 -0300

    Creating a method for filtering per title

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import * as BooksAPI from './utils/BooksAPI'
+import * as BooksAPI from './utils/BooksAPI'
 import './style/App.css'
 import ListBooksReading from './components/ListBooksReading'
 import WantReadBooks from './components/WantReadBooks'
@@ -8,27 +8,27 @@ import ReadBooks from './components/ReadBooks'
 class BooksApp extends React.Component {
 
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    showSearchPage: false,
+    query: '',
+    listBooks: []
   }
 
-  // componentDidMount() {
-  //   const teste = new ListBooksReading
-  //   teste.consultingListBooks()
-  // }
+  updateQuery = query => {
+    this.setState(() => ({
+      query: query.trim()
+    }))
+  }
 
-  // componentDidMount() {
-  //   BooksAPI.getAll()
-  //     .then(res => console.log(res))
-  //     .catch(err => console.warn(err))
-  // }
+  async componentDidMount() {
+    await BooksAPI.getAll().then(list => this.setState(() => ({ listBooks: list })))
+    console.log(this.state.listBooks)
+  }
 
   render() {
+    const { query, listBooks } = this.state
+    const filteredTitle = listBooks.filter(book =>
+      book.title.toLowerCase().includes(query.toLowerCase()))
+
     return (
       <div className="app">
         {this.state.showSearchPage ? (
@@ -36,16 +36,13 @@ class BooksApp extends React.Component {
             <div className="search-books-bar">
               <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author" />
-
+                <input
+                  type="text"
+                  placeholder="Search by title or author"
+                  value={query}
+                  onChange={event => this.updateQuery(event.target.value)}
+                />
+                {filteredTitle.map(book => <p key={book.id}>{book.title}</p>)}
               </div>
             </div>
             <div className="search-books-results">
