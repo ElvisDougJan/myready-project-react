5f291e210d0163cc2620985657ce3cc77767c20e
Elvis Douglas Janegitz <edjelvis@gmail.com>
2019-02-17 21:58:07 -0300

    Adding visualization books

--- src/app/App.js
+++ src/app/App.js
@@ -42,11 +42,37 @@ class BooksApp extends React.Component {
                   value={query}
                   onChange={event => this.updateQuery(event.target.value)}
                 />
-                {filteredTitle.map(book => <p key={book.id}>{book.title}</p>)}
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+                {filteredTitle.map(book => (
+                  <li key={book.id}>
+                    <div className="book">
+                      <div className="book-top">
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                        <div className="book-shelf-changer">
+                          <select>
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="book-title">{book.title}</div>
+                      <div className="book-authors">
+                        {book.authors.map((author, index) => (
+                          <p key={index}>
+                            {author}
+                          </p>
+                        ))}
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ol>
             </div>
           </div>
         ) : (
