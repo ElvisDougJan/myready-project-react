8919a312174fde43e115ea9821d4d96c3544cf8d
Elvis Douglas Janegitz <edjelvis@gmail.com>
2019-02-18 22:38:17 -0300

    Initializing Router

--- src/app/App.js
+++ src/app/App.js
@@ -1,50 +1,20 @@
 import React from 'react'
-import { Router } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import './style/App.css'
-import ListBooksReading from './components/ListBooksReading'
-import WantReadBooks from './components/WantReadBooks'
-import ReadBooks from './components/ReadBooks'
 import SearchBar from './components/SearchBar'
+import Home from './components/Home'
 
-class BooksApp extends React.Component {
-
-  state = {
-    showSearchPage: false
-  }
+export default class BooksApp extends React.Component {
 
   render() {
     return (
       <div>
-        {/* <Router exac path='/search' render={() => (
-          <SearchBar />
-        )} /> */}
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>
-          <div className="list-books-content">
-            <div>
-              <div className="bookshelf">
-                <h2 className="bookshelf-title">Currently Reading</h2>
-                <ListBooksReading />
-              </div>
-              <div className="bookshelf">
-                <h2 className="bookshelf-title">Want to Read</h2>
-                <WantReadBooks />
-              </div>
-              <div className="bookshelf">
-                <h2 className="bookshelf-title">Read</h2>
-                <ReadBooks />
-              </div>
-            </div>
-          </div>
-          <div className="open-search">
-            <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-          </div>
-        </div>
+        <Route exac path='/' component={Home} />
+        <Route path='/search' component={SearchBar} />
+        <Link className="open-search" to='/search'>
+          Add a book
+        </Link>
       </div>
     )
   }
 }
-
-export default BooksApp
---
+++ src/app/components/Home.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import WantReadBooks from './WantReadBooks'
+import ReadBooks from './ReadBooks'
+import ListBooksReading from './ListBooksReading'
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <ListBooksReading />
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <WantReadBooks />
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <ReadBooks />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default Home
