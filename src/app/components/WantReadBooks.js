58bff460dccf644e2d3b03118dfebd96b719c96e
Elvis Douglas Janegitz <elvis@atualcs.com.br>
2019-03-07 09:05:44 -0300

    Changing type inheritence WantReadBooks, ReadBooks and ListBooksReading components from Component to PureComponent

--- src/app/components/WantReadBooks.js
+++ src/app/components/WantReadBooks.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import { updateBook } from './../utils/updateBooks'
 
-export default class WantToRead extends Component {
+export default class WantToRead extends PureComponent {
 
   state = {
     booksWant: []
